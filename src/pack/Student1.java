4dcd2ad3d538e4206a574cba4ced776c84e60e73
vishnupriya <vishnupriya.msk@gmail.com>
2024-04-25 16:15:16 -0400

    constructor overloading

---
+++ src/pack/Student1.java
@@ -0,0 +1,30 @@
+package pack;
+
+public class Student1 {
+	
+	String a;
+	String b;
+
+
+	
+	public Student1 () {
+		
+		System.out.println("Unknow");
+		
+	}
+			
+	public Student1(String a3) {
+		this.a="student name";
+		
+		System.out.println(a);
+		}
+	
+	public Student1(String a1,String b1) {
+		this.a="student name1";
+		this.b="student name2";
+		System.out.println(a + b);
+		
+	}
+	
+
+}
