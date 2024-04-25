bfc271b2324af765068ed4b449ff73f7b65bd776
Aswini <rajyamashwini@gmail.com>
2024-04-25 12:23:09 -0500

    Constructor Assignment

--- src/java_Assignments/MinandMaxValue.java
+++ src/java_Assignments/MinandMaxValue.java
@@ -31,5 +31,10 @@ public class MinandMaxValue {
 		System.out.println(min);
 
 	}
+	public static void main(String[] args) {
+		MinandMaxValue obj=new MinandMaxValue();
+		obj.maximum(new int[]{10, 30, 5, 20, 60, 20 });
+		obj.minimum(new int[]{10, 30, 5, 20, 60, 20 });
 
+}
 }
---
+++ src/java_Assignments/Student.java
@@ -0,0 +1,13 @@
+package java_Assignments;
+
+public class Student {
+	String name;
+	
+	Student(String name){
+		this.name=name;
+	}
+	
+	Student (){
+		this.name = "Unknown";
+	}
+}
