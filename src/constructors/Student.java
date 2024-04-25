17adfe89c4842ccd505c48c75b04ca513c47ae4b
sowjanya1104 <sowjanya1104@gmail.com>
2024-04-25 08:06:42 -0700

    sowjanya changes on constructors

---
+++ src/constructors/Student.java
@@ -0,0 +1,23 @@
+package constructors;
+
+public class Student {
+    
+	String name;
+	
+	public Student()
+	{
+		this.name="unknown";
+		//System.out.println(this.name);
+		
+	}
+	public Student(String name)
+	{
+		this.name="Sowjanya";
+	}
+	public String getName()
+	{
+		return name;
+	}
+
+
+}
