f4014fb071b95c64a86a9d61ad9a8a47ac6f226b
Aswini <rajyamashwini@gmail.com>
2024-04-28 17:19:44 -0500

    CodeFormating

--- src/com/typecasting/TypescastingScanner.java
+++ src/com/typecasting/TypescastingScanner.java
@@ -4,22 +4,16 @@ import java.util.Scanner;
 
 public class TypescastingScanner {
 
-	
-
 	public static void main(String[] args) {
-		
-		Scanner sc=new Scanner(System.in) ;
+
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number");
-		int num=sc.nextInt();
+		int num = sc.nextInt();
 		// converted int to string
-		String numStr=Integer.toString(num);
-		
-		System.out.printf("The number enter by the user:%s",numStr);
-		
-				
-			
-		
-	
+		String numStr = Integer.toString(num);
+
+		System.out.printf("The number enter by the user:%s", numStr);
+
 	}
 
 }
