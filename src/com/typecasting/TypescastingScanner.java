356d85d7017376f8365fce429050187c4b18e73b
Aswini <rajyamashwini@gmail.com>
2024-04-28 11:02:20 -0500

    Added typecasting assignment

---
+++ src/com/typecasting/TypescastingScanner.java
@@ -0,0 +1,25 @@
+package com.typecasting;
+
+import java.util.Scanner;
+
+public class TypescastingScanner {
+
+	
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in) ;
+		System.out.println("Enter the number");
+		int num=sc.nextInt();
+		// converted int to string
+		String numStr=Integer.toString(num);
+		
+		System.out.printf("The number enter by the user:%s",numStr);
+		
+				
+			
+		
+	
+	}
+
+}
