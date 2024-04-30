7f497d16e9ffd2fbdfb445f4dd9308cb5df946f6
himabala11@gmail.com <himabala11@gmail.com>
2024-04-30 14:29:13 -0400

    java

---
+++ src/loopsproject/narrow.java
@@ -0,0 +1,20 @@
+package loopsproject;
+import java.util.Scanner;
+
+public class narrow {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			     Scanner s = new Scanner(System.in);
+			     System.out.println("Enter age: ");
+			     Integer age = s.nextInt();
+			 
+			     long b = (long) age;
+			     System.out.println(b);
+			     System.out.println("Enter age: " +b);
+	                     
+	             
+		}
+
+	}
+
