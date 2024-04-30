e1757057aafcd7c11768ef08bbde18faf40c9bf6
himabala11@gmail.com <himabala11@gmail.com>
2024-04-30 10:04:09 -0400

    loops

---
+++ src/loopsproject/Alpha.java
@@ -0,0 +1,15 @@
+package loopsproject;
+
+public class Alpha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (char a = 'A';a <= 'Z';a++)
+		{
+			System.out.println("alphabet" + " " +a);
+		}
+
+
+	}
+
+}
---
+++ src/loopsproject/Multi.java
@@ -0,0 +1,19 @@
+package loopsproject;
+
+public class Multi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k = 3;
+		   
+	     
+	     for (int i = 1;i < 11;i++)
+	     {
+	    		 int out = k * i;
+	    		 System.out.println(k + "*" + i + "=" +out);
+	   }
+
+
+	}
+
+}
